b15c6b676cb0c1d5f1ab0a3534e59bf0c5932b94
TREINAMENTO26 <TREINAMENTO26@TREINAMENTO26N>
2021-11-05 10:28:55 -0300

    Adicionar arquivos de projeto.

---
+++ SquadManeger/Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SquadManeger.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SquadManeger.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            UserViewModel user = new UserViewModel();
+
+            return View("index", user);
+        }
+        [HttpPost]
+        public IActionResult Test(UserViewModel user)
+        {
+            user.Email = "Email enviado";
+
+            return View("index",user);
+        }
+    }
+}
---
+++ SquadManeger/Models/UserViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SquadManeger.Models
+{
+    public class UserViewModel
+    {
+        public String Username { get; set; }
+        public String Email { get; set; }
+        public String Password { get; set; }
+    }
+}
